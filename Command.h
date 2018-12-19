c81de8668fe48acc10f8e0833a7dfa5056baed97
Neriya Shulman <neriyashul@gmail.com>
2018-12-19 22:57:48 +0200

    add Command interfe

---
+++ Command.h
@@ -0,0 +1,15 @@
+#ifndef MILE_STONE1_COMMAND_H
+#define MILE_STONE1_COMMAND_H
+
+#include <string>
+using namespace std;
+
+// interface
+class Commamd {
+public:
+
+    virtual void doCommand(string);
+};
+
+
+#endif //MILE_STONE1_COMMAND_H
