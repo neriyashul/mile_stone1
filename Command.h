6154b9e052dcef2389e604d6cf00c158fe44c638
Neriya Shulman <neriyashul@gmail.com>
2018-12-20 00:45:51 +0200

    add Commit

---
+++ Command.h
@@ -0,0 +1,15 @@
+#ifndef MILE_STONE1_COMMAND_H
+#define MILE_STONE1_COMMAND_H
+
+#include <string>
+using namespace std;
+
+// interface
+class Command {
+public:
+
+    virtual void doCommand(string) = 0;
+};
+
+
+#endif //MILE_STONE1_COMMAND_H
